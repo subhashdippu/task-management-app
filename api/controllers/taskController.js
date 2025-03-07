cc1640f4012d41eade2d13f7a49337ec98b44233
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-03-07 23:15:33 +0530

    getting task detail

--- api/controllers/taskController.js
+++ api/controllers/taskController.js
@@ -24,4 +24,13 @@ const createTask = async (req, res) => {
   }
 };
 
-module.exports = { createTask };
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({ userId: req.user.id });
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { createTask, getTasks };
--- api/routes/taskRoutes.js
+++ api/routes/taskRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createTask } = require("../controllers/taskController");
+const { createTask, getTasks } = require("../controllers/taskController");
 const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/", protect, createTask);
+router.get("/", protect, getTasks);
 
 module.exports = router;
