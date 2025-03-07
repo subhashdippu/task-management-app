8eec00c57108290e4fd2ac0e9dcd1c1ed0aa6a68
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-03-07 10:40:33 +0530

    connecting mongodb

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
-
+const connectDB = require("./api/config/db");
+const dotenv = require("dotenv");
 const app = express();
 
+dotenv.config();
+connectDB();
 const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
